ae5a48a44c7fa432b49c089426be9bc56419cf7d
Wisarud2k <59276807+Wisarud2k@users.noreply.github.com>
2021-02-06 21:11:25 +0700

    try to deploy

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const { join } = require("path");
 //to post you must use bodyParser
 
 app.use(express.static("assets"));
-app.use(bodyParser.json("assets"));  // check again when fo post
-//app.use(bodyParser.json)
+// need to implement diff from example from p'mak because we use js not ts
+app.use(bodyParser.json("assets"));  
+
 
 app.get("/", (req, res) => {
   res.end(fs.readFileSync("./instruction.html"));
@@ -17,9 +18,10 @@ app.get("/", (req, res) => {
 //implement your api here
 //follow instruction in http://localhost:8000/
 
-// const flie = fs.readFileSync('myCourses.json','utf8')
 
+// normal get 
 app.get("/courses",(req,res) => {
+  //file is string need to parse to json (data)
   const file = fs.readFileSync('myCourses.json','utf8')
   const data = JSON.parse(file)
   res.json(data)
@@ -31,8 +33,10 @@ app.get("/courses/:id",(req,res) => {
   const file = fs.readFileSync('myCourses.json','utf8')
   const data = JSON.parse(file)
   //res.json(data.courses[1].courseId);
+  //use callback function find (from video week 8)
   const courseid = data.courses.find(date => date.courseId === Number(id))
-  if(!courseid){
+  //check if we have this id return in json form if not return status 404 not found
+  if(!courseid){  
     res.status(404)
     res.json({message: `courseId: ${id} not found`})
     return
@@ -46,9 +50,10 @@ app.delete("/courses/:id",(req,res) => {
   // console.log(id);
   const file = fs.readFileSync('myCourses.json','utf8')
   const data = JSON.parse(file)
-
+  // use filter to get every course in data except theone we need to delete
   data.courses = data.courses.filter(date => date.courseId !== Number(id))
 
+  ///////cal grade////////
   let sum = 0;
   let i = 0;
   data.courses.map(date => {
@@ -58,22 +63,25 @@ app.delete("/courses/:id",(req,res) => {
   })
   // console.log(sum/i)
   data.gpax = (sum/i);
+  ///////cal grade////////
+  // write to mycourse.json because we delete and re cal gpax
   fs.writeFileSync('myCourses.json',JSON.stringify(data))
   res.json(data)
 
 })
 
 app.post("/addCourse",(req,res) => {
-  const course = req.body
+  const course = req.body //course is come from body we provide
   const file = fs.readFileSync('myCourses.json','utf8')
   const data = JSON.parse(file)
-
+  //if one of course component is missing return status 422
   if(course.courseId === null || course.courseName === null ||
     course.credit === null || course.gpa === null){
       res.status(422);
       res.json({message: 'data not complete'})
+      return
     }
-
+  //if not we push this course to data
   data.courses.push({
     ...course,
   })
@@ -86,6 +94,7 @@ app.post("/addCourse",(req,res) => {
     //console.log(date.gpa)
   })
   data.gpax = (sum/i);
+  
   fs.writeFileSync('myCourses.json',JSON.stringify(data))
   res.json(course)
 })
